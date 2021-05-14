bd781b9caff97669a301215f252fe32a52d6d489
DannJohnrem <41779963+DannJohnrem@users.noreply.github.com>
2021-05-14 19:07:59 +0800

    Update MyProfileModel.cs

--- WebApi2/Models/MyProfileModel.cs
+++ WebApi2/Models/MyProfileModel.cs
@@ -8,7 +8,9 @@ namespace WebApi2.Models
     public class MyProfileModel
     {
         public int id { get; set; }
+        public int gender_id { get; set; }
         public string first_name { get; set; }  
         public string last_name { get; set; }
+        public string gender { get; set; }
     }
 }
